f042a42fd0ea86feb6fa30db2d6f31d37626b321
Ashutosh tripathi <ashutripathi.rajababu@gmail.com>
2024-05-11 14:51:01 +0530

    tkinter and os are import in this project

---
+++ gui.py
@@ -0,0 +1,39 @@
+from tkinter import*
+import os
+
+def sleep():
+    os.system("shutdown /r /t 1")
+
+def  restart_time():
+    os.system("shutdown /r /t 20")
+
+def logout():
+    os.system("shutdown /r /t -1")
+
+def shutdown():
+    os.system("shutdown /r /t 1")
+
+st = Tk()
+st.title("ShutDown App")
+st.geometry("500x500")
+st.config(bg="Blue")
+
+r_button = Button(st,text="Restart",font=("Time New Roman",30, "bold"), relief=RAISED,cursor="plus",command=sleep)
+r_button.place(x=150,y=60 ,height=50,width=200)
+
+rt_button = Button(st,text="Restart Time",font=("Time New Roman",20, "bold"), relief=RAISED,cursor="plus", command=restart_time)
+rt_button.place(x=150,y=170 ,height=50,width=200)
+
+
+lg_button = Button(st,text="Log-out",font=("Time New Roman",20, "bold"), relief=RAISED,cursor="plus",command=logout)
+lg_button.place(x=150,y=270 ,height=50,width=200)
+
+
+st_button = Button(st,text="Log-out",font=("Time New Roman",20, "bold"), relief=RAISED,cursor="plus" ,command=shutdown)
+st_button.place(x=150,y=370 ,height=50,width=200)
+
+
+
+st.mainloop()
+
+
